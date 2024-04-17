53657c49f8cd375660f905e60fac223efb445a8f
tanmaysihare <tanmaysihare@gmail.com>
2024-04-18 03:06:51 +0530

    minor

--- controllers/User.js
+++ controllers/User.js
@@ -46,7 +46,7 @@ exports.loginUser = async (req, res) => {
         }
         const accessToken = createTokens(user);
         await transaction.commit();
-        return res.status(200).json({message: "Login successful", accessToken: accessToken});
+        return res.status(200).json({success: true, message: "Login successful", accessToken: accessToken});
     }catch(error){
         if(transaction) await transaction.rollback();
         return res.status(500).json({message: error.message});
